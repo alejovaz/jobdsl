3691285dd468d32234107ceebc31e9d4e367a4f6
Jose Alejandro Vazquez <alejo.vazquez@gmail.com>
2020-02-03 15:42:12 -0300

    Agrega vista python

---
+++ jobs/views.groovy
@@ -0,0 +1,16 @@
+
+listView('python') {
+	description('Python Pipelines')
+	jobs {
+		regex(/.*python.*/)
+	}
+}
+columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+}
